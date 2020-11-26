3c0456bd6478a31a4897ab133ed19045c2630972
donghwi <ehdehd1289@naver.com>
2020-11-26 18:01:30 +0900

    youtube clone coding

---
+++ main.js
@@ -0,0 +1,7 @@
+const moreBtn = document.querySelector('.info .metadata .titleAndButton .moreBtn');
+const title = document.querySelector('.info .metadata .titleAndButton .title');
+
+moreBtn.addEventListener('click' , () => {
+  moreBtn.classList.toggle('clicked');
+  title.classList.toggle('clamp');
+})
